aae05a2af5244432109ab8cc2d104b0118ae5ad3
David <hexod0t@protonmail.com>
2024-05-06 00:56:00 -0500

    Add job matcher service

---
+++ internal/prompt_template.py
@@ -0,0 +1,14 @@
+input_prompt = """
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide
+best assistance for improving thr resumes. Assign the percentage Matching based
+on Job description and
+the missing keywords with high accuracy
+
+I want the response as per below structure
+{"job_description_match": "%",
+"missing_keywords": [],
+"profile_summary": ""}
+"""
---
+++ services/job_matcher.py
@@ -0,0 +1,84 @@
+"""
+Matcher Module
+"""
+
+import re
+import json
+import google.generativeai as genai
+from fastapi import HTTPException
+from internal.prompt_template import input_prompt
+from internal.config import get_settings
+from google.api_core import exceptions as api_errors
+
+
+settings = get_settings()
+
+genai.configure(api_key=settings.gemini_api_key)
+
+
+def get_match(*, job_description: str, resume_text: str):
+    """Get the results of the match analysis between the
+    job description and the resume text of the candidate
+
+    Paramters
+    -----------
+    resume_text
+        the text extracted from the candidate's resume
+    job description
+        the job description to be compared with the candidate's resume
+
+    """
+    model = genai.GenerativeModel("gemini-pro")
+
+    optimized_prompt = [
+        input_prompt,
+        "Job Description\n" + job_description,
+        "Resume \n" + resume_text,
+    ]
+
+    try:
+        response = model.generate_content(optimized_prompt)
+    except (api_errors.InternalServerError, api_errors.ResourceExhausted) as error:
+        raise HTTPException(status_code=400, detail=error.response["Error"])
+
+    return _extract_json(response.text)
+
+
+def _extract_json(text_response):
+    """Extract the json from the generated text"""
+    pattern = r"\{[^{}]*\}"
+
+    matches = re.finditer(pattern, text_response)
+    json_objects = []
+
+    for match in matches:
+        json_str = match.group(0)
+        try:
+            json_obj = json.loads(json_str)
+            return json_obj
+        except json.JSONDecodeError:
+            extended_json_str = _extend_search(text_response, match.span())
+            try:
+                json_obj = json.loads(extended_json_str)
+                return json_obj
+            except json.JSONDecodeError:
+                continue
+
+    if json_objects:
+        return json_objects
+    else:
+        return None
+
+
+def _extend_search(text, span):
+    """Extend search in the the generated text"""
+    start, end = span
+    nest_count = 0
+    for i in range(start, len(text)):
+        if text[i] == "{":
+            nest_count += 1
+        elif text[i] == "}":
+            nest_count -= 1
+            if nest_count == 0:
+                return text[start : i + 1]
+    return text[start:end]
