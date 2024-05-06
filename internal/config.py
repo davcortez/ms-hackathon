2ba6bdafe8d7e2d188dc68392173b25496ba2614
David <hexod0t@protonmail.com>
2024-05-06 00:22:54 -0500

    Add configuration file to handle environment vars

---
+++ internal/config.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str = "Match API"
+
+    environment: str
+    gemini_api_key: str
+
+    # cors_allow_credentials: bool = False
+    # cors_http_methods: list
+    # cors_headers: list
+    # cors_origins: list
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
