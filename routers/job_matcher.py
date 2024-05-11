b07b9e26d6f3ab62dde6485fdbbe48b2b11561a5
David <hexod0t@protonmail.com>
2024-05-11 02:46:04 -0500

    Add async service to call Gemini API

--- routers/job_matcher.py
+++ routers/job_matcher.py
@@ -1,6 +1,7 @@
+import json
 from fastapi import APIRouter, status, Security
 from schemas.job_match import JobMatchRequest, JobMatchResponse
-from services.job_matcher import get_match
+from services.job_matcher import generate_content_async
 from internal.auth import get_token
 
 router = APIRouter(prefix="/v1", dependencies=[Security(get_token)], tags=["main"])
@@ -9,11 +10,27 @@ router = APIRouter(prefix="/v1", dependencies=[Security(get_token)], tags=["main
 @router.post(
     "/job-matcher", status_code=status.HTTP_200_OK, response_model=JobMatchResponse
 )
-def get_match_response(payload: JobMatchRequest):
+async def get_match_response(payload: JobMatchRequest):
     """
     return the results of the matching between
     the job description and the candidate's resume
     """
-    match_response = get_match(payload=payload)
+    match_response = await generate_content_async(payload=payload)
+    response = _extract_data_v2(data=match_response)
 
-    return match_response
+    return response
+
+
+def _extract_data_v2(*, data):
+    """Extract data form REST API Gemini"""
+    candidates_data = {}
+    for candidate in data["candidates"]:
+        content = candidate["content"]["parts"][0]["text"]
+        content_dict = json.loads(content)
+        candidates_data["job_description_match"] = content_dict.get(
+            "job_description_match"
+        )
+        candidates_data["matching_keywords"] = content_dict.get("matching_keywords")
+        candidates_data["profile_summary"] = content_dict.get("profile_summary")
+
+    return candidates_data
--- services/job_matcher.py
+++ services/job_matcher.py
@@ -6,6 +6,7 @@ import re
 import json
 import google.generativeai as genai
 from fastapi import HTTPException
+import aiohttp
 from internal.prompt_template import input_prompt
 from internal.config import get_settings
 from google.api_core import exceptions as api_errors
@@ -17,6 +18,35 @@ settings = get_settings()
 genai.configure(api_key=settings.gemini_api_key)
 
 model_config = {"temperature": 0.3, "top_p": 1, "top_k": 32, "max_output_tokens": 4096}
+
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key={settings.gemini_api_key}"
+
+
+async def generate_content_async(*, payload: JobMatchRequest):
+    """Asynchronous service to call Gemini API"""
+    async with aiohttp.ClientSession() as session:
+        payload = {
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": f"{input_prompt}\n Job Description\n {payload.job_description} Resume \n {payload.resume_text}"
+                        }
+                    ]
+                }
+            ],
+            "generationConfig": {
+                "response_mime_type": "application/json",
+                "temperature": 0.3,
+                "maxOutputTokens": 4096,
+                "topP": 1,
+                "topK": 32,
+            },
+        }
+
+        async with session.post(GEMINI_URL, json=payload) as response:
+            response.raise_for_status()
+            return await response.json()
 
 
 def get_match(*, payload: JobMatchRequest):
