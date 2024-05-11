83958765ae863a87bbdb06d3dc93db46ea0788c5
David <hexod0t@protonmail.com>
2024-05-11 03:40:45 -0500

    Return to the sync way

--- routers/job_matcher.py
+++ routers/job_matcher.py
@@ -1,7 +1,6 @@
-import json
 from fastapi import APIRouter, status, Security
 from schemas.job_match import JobMatchRequest, JobMatchResponse
-from services.job_matcher import generate_content_async
+from services.job_matcher import get_match
 from internal.auth import get_token
 
 router = APIRouter(prefix="/v1", dependencies=[Security(get_token)], tags=["main"])
@@ -10,27 +9,11 @@ router = APIRouter(prefix="/v1", dependencies=[Security(get_token)], tags=["main
 @router.post(
     "/job-matcher", status_code=status.HTTP_200_OK, response_model=JobMatchResponse
 )
-async def get_match_response(payload: JobMatchRequest):
+def get_match_response(payload: JobMatchRequest):
     """
     return the results of the matching between
     the job description and the candidate's resume
     """
-    match_response = await generate_content_async(payload=payload)
-    response = _extract_data_v2(data=match_response)
+    match_response = get_match(payload=payload)
 
-    return response
-
-
-def _extract_data_v2(*, data):
-    """Extract data form REST API Gemini"""
-    candidates_data = {}
-    for candidate in data["candidates"]:
-        content = candidate["content"]["parts"][0]["text"]
-        content_dict = json.loads(content)
-        candidates_data["job_description_match"] = content_dict.get(
-            "job_description_match"
-        )
-        candidates_data["matching_keywords"] = content_dict.get("matching_keywords")
-        candidates_data["profile_summary"] = content_dict.get("profile_summary")
-
-    return candidates_data
+    return match_response
